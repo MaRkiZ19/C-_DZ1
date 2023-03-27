4435d534910aeda1e20e3501d6326323d5d0e987
ваше_имя <grigorenko-marko@rambler.ru>
2023-03-27 22:43:26 +0300

    f..ck brain task04

---
+++ task04/Program.cs
@@ -0,0 +1,54 @@
+﻿//Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+int N = 0;
+System.Console.WriteLine("add N ");
+N = int.Parse(Console.ReadLine());
+/*
+int [] array = new int [N];
+int index = 0;
+int A = 1;
+
+while (A <= N)
+{
+    if ( A % 2 == 0)
+    {
+        array[index] = A;
+    }
+    int A = A+1;
+    int index = index + 1;   
+}
+Console.ReadLine(array);
+
+
+
+void FillArray(int[] even )
+{
+    int length = even.Length;
+    int index = 0;
+    while (index < ltngth) 
+    {
+        if ( A % 2 == 0)
+       
+        even[index] = A;
+        int A = A+1;
+        index++;
+    }
+}
+
+
+
+int a = 1;
+int [] even= new int [N];
+int index = 0;
+while (a <= N)
+{
+    if (a % 2 == 0)
+    {
+        even [index] = a;
+    }
+}
+FillArray(even);
+PrintArray(even);
+System.Console.WriteLine();
+
+*/
