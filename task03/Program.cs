aeeed7322a1b5f156b4dd367c5cc37099b0d7904
ваше_имя <grigorenko-marko@rambler.ru>
2023-03-27 20:40:35 +0300

    add task03

--- task03/Program.cs
+++ task03/Program.cs
@@ -1,4 +1,14 @@
 ﻿//Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
 
+int N = 0;
+System.Console.WriteLine("add N ");
+N = int.Parse(Console.ReadLine());
 
-
+if (N%2==0)
+{
+    System.Console.WriteLine("Even");
+}
+else
+{
+    System.Console.WriteLine("not even");
+}
